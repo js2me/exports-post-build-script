f36c3f9791dba3ba0e03f66cd911b18ed63edd48
Sergey Volkov <js2me@outlook.com>
2025-07-05 12:15:06 +0300

    feat: publish gh release

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './post-build-script.js';
 export * from './publish-script.js';
+export * from './publish-gh-release.js';
 export * from './get-info-from-changelog.js';
---
+++ src/publish-gh-release.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
+import ghRelease from 'gh-release/index.js';
+
+interface PublishGhReleaseConfig {
+  version: string;
+  tag_name: string;
+  name: string;
+  body: string;
+  draft?: boolean;
+  prerelease?: boolean;
+  repo: string;
+  owner: string;
+  authToken: string;
+}
+
+export const publishGhRelease = (config: PublishGhReleaseConfig) => {
+  return new Promise((resolve, reject) => {
+    ghRelease(
+      {
+        tag_name: config.version,
+        name: config.version,
+        body: config.body,
+        draft: config.draft ?? false,
+        prerelease: config.prerelease ?? false,
+        repo: config.repo,
+        owner: config.owner,
+        auth: {
+          token: config.authToken,
+        },
+      },
+      (err: any, result: any) => {
+        if (err) reject(err);
+        resolve(result);
+      },
+    );
+  });
+};
