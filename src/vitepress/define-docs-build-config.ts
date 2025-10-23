38cfcbf9f921f9ba4b6d9120bf59c622ab65c297
Sergey Volkov <js2me@outlook.com>
2025-10-23 18:51:57 +0300

    feat: publish v1.2.0 version

---
+++ src/vitepress/define-docs-build-config.ts
@@ -0,0 +1,62 @@
+import { presetAttributify, presetIcons, presetUno } from 'unocss';
+import Unocss from 'unocss/vite';
+import { defineConfig, mergeConfig } from 'vite';
+import type { DefaultTheme, UserConfig } from 'vitepress';
+import type { ConfigsManager } from '../utils/configs-manager.js';
+
+export const defineDocsBuildConfig = (
+  configs: ConfigsManager,
+  config?: UserConfig<DefaultTheme.Config>,
+) => {
+  const definedConfig = defineConfig({
+    optimizeDeps: {
+      exclude: ['@vueuse/core', 'vitepress'],
+    },
+    server: {
+      hmr: {
+        overlay: false,
+      },
+    },
+    define: {
+      __PACKAGE_DATA__: JSON.stringify(configs.package),
+    },
+    plugins: [
+      {
+        name: 'replace-package-json-vars',
+        transform(code, id) {
+          if (!id.endsWith('.md')) return;
+          return code.replace(/\{packageJson\.(\w+)\}/g, (_, key) => {
+            return configs.package[key] || '';
+          });
+        },
+      },
+      {
+        name: 'replace-source-links',
+        transform(code, id) {
+          if (!id.endsWith('.md')) return;
+          return code.replace(
+            /(\(\/src\/)/g,
+            `(https://github.com/${configs.package.author}/${configs.package.name}/tree/master/src/`,
+          );
+        },
+      },
+      Unocss({
+        presets: [
+          presetUno({
+            dark: 'media',
+          }),
+          presetAttributify(),
+          presetIcons({
+            scale: 1.2,
+          }),
+        ],
+      }),
+    ],
+  });
+
+  if (config) {
+    return mergeConfig(definedConfig, config);
+  }
+
+  return definedConfig;
+};
--- src/vitepress/index.ts
+++ src/vitepress/index.ts
@@ -1,1 +1,2 @@
+export * from './define-docs-build-config.js';
 export * from './define-gh-pages-doc-config.js';
